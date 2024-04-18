40f9c396d411407b3061372e1ad017702ce66ecc
gusrl123456 <gusrl123456@gmail.com>
2024-04-19 08:56:46 +0900

    [Algo] boj : 17352

---
+++ src/boj_17352/Main.java
@@ -0,0 +1,54 @@
+package boj_17352;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] par;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        par = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            par[i] = i;
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 2; i <= N; i++) {
+            if (find(1) != find(i)) {
+                System.out.println(1 + " " + i);
+                return;
+            }
+        }
+    }
+
+    private static int find(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x < y) {
+            par[y] = x;
+        } else {
+            par[x] = y;
+        }
+    }
+}
