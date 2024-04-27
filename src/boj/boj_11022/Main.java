06fe2cc21c236ad73883449e66d7a7222eb6a45c
ggobug <ggobug94@gmail.com>
2024-04-27 09:20:09 +0900

    [Algo] boj : 11022

---
+++ src/boj/boj_11022/Main.java
@@ -0,0 +1,25 @@
+package boj.boj_11022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int t = 0 ; t < T ; t++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append("Case #" + t + ": ").append(a + " + " + b + " = " + (a + b) + "\n");
+        }
+        System.out.println(sb);
+    }
+}
