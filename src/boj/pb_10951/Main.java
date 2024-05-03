c48d684a2c0a72e05c062d1d317f06070c6231cf
ggobug <ggobug94@gmail.com>
2024-05-03 20:29:31 +0900

    [Algo] boj : 10951

---
+++ src/boj/pb_10951/Main.java
@@ -0,0 +1,25 @@
+package boj.pb_10951;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        String temp;
+        while (true) { // 파일의 끝까지 입력을 받음
+            temp = br.readLine();
+            if (temp == null) break;
+            st = new StringTokenizer(temp);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
