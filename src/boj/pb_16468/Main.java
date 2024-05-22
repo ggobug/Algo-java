8b1026149cdaae9a85b5b6e027cbf0d4fe2595b5
ggobug <ggobug94@gmail.com>
2024-05-22 17:50:46 +0900

    [Algo] bojg : temp

---
+++ src/boj/pb_16468/Main.java
@@ -0,0 +1,20 @@
+package boj.pb_16468;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 볼의 개수
+        int L = Integer.parseInt(br.readLine());    // 트리의 높이
+
+        // 트리를 꾸밀 수 있는 경우의 수
+        // 깊이와 노드의 개수가 정해져 있을 때
+
+        int mod = 100_030_001;
+
+
+    }
+}
