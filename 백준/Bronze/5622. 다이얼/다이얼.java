644eafa9b0740e8bb140b4568bfd3f7f66417a21
최현기 <139421105+ggobug@users.noreply.github.com>
2024-06-23 15:19:16 +0900

    [Bronze II] Title: 다이얼, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int totalTime = 0;
+
+        for (char ch : input.toCharArray()) {
+            if (ch >= 'A' && ch <= 'C') {
+                totalTime += 3;
+            } else if (ch >= 'D' && ch <= 'F') {
+                totalTime += 4;
+            } else if (ch >= 'G' && ch <= 'I') {
+                totalTime += 5;
+            } else if (ch >= 'J' && ch <= 'L') {
+                totalTime += 6;
+            } else if (ch >= 'M' && ch <= 'O') {
+                totalTime += 7;
+            } else if (ch >= 'P' && ch <= 'S') {
+                totalTime += 8;
+            } else if (ch >= 'T' && ch <= 'V') {
+                totalTime += 9;
+            } else if (ch >= 'W' && ch <= 'Z') {
+                totalTime += 10;
+            }
+        }
+
+        System.out.println(totalTime);    
+    }
+}
