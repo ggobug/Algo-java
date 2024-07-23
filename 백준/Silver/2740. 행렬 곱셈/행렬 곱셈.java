c157c9731cdbd675989748bce2e9c756bc72462b
최현기 <139421105+ggobug@users.noreply.github.com>
2024-07-23 13:36:43 +0900

    [Silver V] Title: 행렬 곱셈, Time: 196 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, K;
+    static int[][] matrixA, matrixB;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        matrixA = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                matrixA[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        matrixB = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                matrixB[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] answer = productMatrix(matrixA, matrixB);
+        StringBuilder sb = new StringBuilder();
+        for (int[] ints : answer) {
+            for (int anInt : ints) {
+                sb.append(anInt).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int[][] productMatrix(int[][] A, int[][] B) {
+        int r = A.length;
+        int c = B[0].length;
+        int[][] C = new int[r][c];
+
+        for (int j = 0; j < c; j++) {
+            for (int i = 0; i < r; i++) {
+                for (int k = 0; k < B.length; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+}
