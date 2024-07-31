1e481fc598b8420dbb110eeee430ea69886ec4a4
최현기 <139421105+ggobug@users.noreply.github.com>
2024-07-31 16:28:15 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        System.out.println(Math.min(Math.min(x, Math.abs(w - x)), Math.min(y, Math.abs(h - y))));
+    }
+}
