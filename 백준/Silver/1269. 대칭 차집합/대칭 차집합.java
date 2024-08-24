be1a9da01db8f47e066f9a9006f23b3e212ca762
최현기 <139421105+ggobug@users.noreply.github.com>
2024-08-24 16:03:38 +0900

    [Silver IV] Title: 대칭 차집합, Time: 604 ms, Memory: 80020 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -13,34 +13,18 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        // 공집합이 아닌 두 집합
-        Set<Integer> A = new HashSet<>();
-        Set<Integer> B = new HashSet<>();
+        Set<Integer> C = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < a; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
+            C.add(Integer.parseInt(st.nextToken()));
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < b; i++) {
-            B.add(Integer.parseInt(st.nextToken()));
+            C.add(Integer.parseInt(st.nextToken()));
         }
 
-        // 두 집합의 대칭 차집합의 원소의 개수를 출력
-
-        // A - B
-        Set<Integer> C = new HashSet<>(A);
-        C.removeAll(B);
-
-        // B - A
-        Set<Integer> D = new HashSet<>(B);
-        D.removeAll(A);
-
-        // 대칭 차집합
-        C.addAll(D);
-
-        // 개수 출력
-        System.out.println(C.size());
+        System.out.println(2 * C.size() - (a + b));
     }
 }
