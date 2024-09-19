ab5a6d920b1aad66001fc6e33aef15aaede180fb
최현기 <139421105+ggobug@users.noreply.github.com>
2024-09-20 00:52:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15472 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,69 +4,67 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int N;
-    static int[] numbers;
-    static int[] operators = new int[4]; // 덧셈, 뺄셈, 곱셈, 나눗셈
-    static int maxResult = Integer.MIN_VALUE;
-    static int minResult = Integer.MAX_VALUE;
+    static int[] operators, seq;
+    static long maxValue = Long.MIN_VALUE;
+    static long minValue = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // N 입력
         N = Integer.parseInt(br.readLine());
 
-        // 숫자 배열 입력
-        numbers = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        seq = new int[N]; // N개의 수열
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 연산자 입력 (덧셈, 뺄셈, 곱셈, 나눗셈 순서)
         st = new StringTokenizer(br.readLine());
+        operators = new int[4];   // + - x / 의 개수
         for (int i = 0; i < 4; i++) {
             operators[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 백트래킹을 통해 최대값과 최소값 찾기
-        backtrack(numbers[0], 1);
+        // 만들 수 있는 연산 결과값의 최댓값, 최솟값 구하기
+        calculate(seq[0], 1);
 
-        // 결과 출력
-        System.out.println(maxResult);
-        System.out.println(minResult);
+        System.out.println(maxValue);
+        System.out.println(minValue);
     }
 
-    // 백트래킹 함수
-    public static void backtrack(int currentResult, int idx) {
-        if (idx == N) { // 모든 수를 다 사용한 경우
-            maxResult = Math.max(maxResult, currentResult);
-            minResult = Math.min(minResult, currentResult);
+    static void calculate(long curValue, int idx) {
+
+        // 모든 연산자를 사용한 경우
+        if (idx >= N) {
+            // 최댓값, 최솟값
+            maxValue = Math.max(maxValue, curValue);
+            minValue = Math.min(minValue, curValue);
             return;
         }
 
-        // 각 연산자에 대해 가능한 경우를 재귀적으로 탐색
         for (int i = 0; i < 4; i++) {
-            if (operators[i] > 0) {
-                operators[i]--; // 연산자 사용
-
+            if (operators[i] > 0) { // 사용할 수 있는 연산자가 남아있으면
+                operators[i]--;
                 switch (i) {
-                    case 0: // 덧셈
-                        backtrack(currentResult + numbers[idx], idx + 1);
+                    case 0:
+                        calculate(curValue + (long) seq[idx], idx + 1);
                         break;
-                    case 1: // 뺄셈
-                        backtrack(currentResult - numbers[idx], idx + 1);
+                    case 1:
+                        calculate(curValue - (long) seq[idx], idx + 1);
                         break;
-                    case 2: // 곱셈
-                        backtrack(currentResult * numbers[idx], idx + 1);
+                    case 2:
+                        calculate(curValue * (long) seq[idx], idx + 1);
                         break;
-                    case 3: // 나눗셈
-                        backtrack(currentResult / numbers[idx], idx + 1);
+                    case 3:
+                        calculate(curValue / (long) seq[idx], idx + 1);
                         break;
                 }
-
-                operators[i]++; // 연산자 복구
+                operators[i]++;
             }
         }
     }
+
+
+
 }
