733e74c848272f32f79b1eb2e94f7750b9731f61
최현기 <139421105+ggobug@users.noreply.github.com>
2024-10-16 22:40:29 +0900

    [Gold IV] Title: 점수따먹기, Time: 316 ms, Memory: 21208 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -1,6 +1,3 @@
-// 점수따먹기
-// https://www.acmicpc.net/problem/1749
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,31 +22,19 @@ public class Main {
             }
         }
 
-        // 누적합 구하기
-        int[][] accum = new int[N + 1][M + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                accum[i][j] = matrices[i - 1][j - 1]
-                        + accum[i-1][j]
-                        + accum[i][j-1]
-                        - accum[i-1][j-1];
-            }
-        }
-
         int maxSum = Integer.MIN_VALUE; // 최대 구간 합
 
         // 행을 기준으로 구간합 계산
-        for (int i1 = 1; i1 <= N; i1++) {
-            for (int i2 = i1; i2 <= N; i2++) {
-                // 열에 대한 구간 합을 저장하는 배열
-                int[] colSum = new int[M + 1];
-                for (int j = 1; j <= M; j++) {
-                    colSum[j] = accum[i2][j] - accum[i1 - 1][j] - accum[i2][j-1] + accum[i1-1][j-1];
+        for (int i1 = 0; i1 < N; i1++) {
+            int[] colSum = new int[M]; // 열에 대한 구간 합을 저장하는 배열
+
+            for (int i2 = i1; i2 < N; i2++) {
+                for (int j = 0; j < M; j++) {
+                    colSum[j] += matrices[i2][j];
                 }
 
-                // 슬라이딩 윈도우로 1차원 최대 구간 합을 구함
                 int currentSum = 0;
-                for (int j = 1; j <= M; j++) {
+                for (int j = 0; j < M; j++) {
                     currentSum = Math.max(colSum[j], currentSum + colSum[j]);
                     maxSum = Math.max(maxSum, currentSum);
                 }
