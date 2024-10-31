07805aac0beab1dee11a95bd8704ba8b961c5a54
최현기 <139421105+ggobug@users.noreply.github.com>
2024-10-31 13:18:58 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 652 ms, Memory: 117956 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int count = 0;
+    static int K;
+    static int result = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // A 배열의 크기
+        K = Integer.parseInt(st.nextToken());
+
+        // 배열 입력 받기
+        int[] arrayA = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arrayA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(arrayA, 0, N - 1);
+
+        System.out.println(result);
+    }
+
+    // 병합정렬 함수(오름차순 정렬)
+    static void mergeSort(int[] arr, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(arr, p, q);          // 전반부 정렬
+            mergeSort(arr, q + 1, r);   // 후반부 정렬
+            merge(arr, p, q, r);           // 병합
+        }
+    }
+
+    // 병합 함수
+    static void merge(int[] arr, int p, int q, int r) {
+        int[] tmp = new int[r - p + 1];
+        int i = p, j = q + 1, t = 0;
+
+        // 두 부분 배열을 병합
+        while (i <= q && j <= r) {
+            if (arr[i] <= arr[j]) {
+                tmp[t++] = arr[i++];
+            } else {
+                tmp[t++] = arr[j++];
+            }
+        }
+
+        // 남은 요소 처리
+        while (i <= q) {
+            tmp[t++] = arr[i++];
+        }
+        while (j <= r) {
+            tmp[t++] = arr[j++];
+        }
+
+        // 병합 결과를 원래 배열에 저장
+        for (i = p, t = 0; i <= r; i++, t++) {
+            arr[i] = tmp[t];
+            count++;
+            if (count == K) { // K번째 저장이면 결과 저장
+                result = arr[i];
+            }
+        }
+    }
+}
