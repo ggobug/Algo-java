e9efc87da1d99b91fa20a3130bca8bb4f1c734a9
최현기 <139421105+ggobug@users.noreply.github.com>
2024-10-31 13:20:13 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 588 ms, Memory: 73576 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -7,11 +7,12 @@ public class Main {
     static int count = 0;
     static int K;
     static int result = -1;
+    static int[] tmp; // tmp 배열을 전역으로 선언하여 재사용
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());   // A 배열의 크기
+        int N = Integer.parseInt(st.nextToken()); // A 배열의 크기
         K = Integer.parseInt(st.nextToken());
 
         // 배열 입력 받기
@@ -21,25 +22,26 @@ public class Main {
             arrayA[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 임시 배열 초기화
+        tmp = new int[N];
         mergeSort(arrayA, 0, N - 1);
 
         System.out.println(result);
     }
 
-    // 병합정렬 함수(오름차순 정렬)
+    // 병합 정렬 함수(오름차순 정렬)
     static void mergeSort(int[] arr, int p, int r) {
         if (p < r) {
             int q = (p + r) / 2;
-            mergeSort(arr, p, q);          // 전반부 정렬
+            mergeSort(arr, p, q);       // 전반부 정렬
             mergeSort(arr, q + 1, r);   // 후반부 정렬
-            merge(arr, p, q, r);           // 병합
+            merge(arr, p, q, r);        // 병합
         }
     }
 
     // 병합 함수
     static void merge(int[] arr, int p, int q, int r) {
-        int[] tmp = new int[r - p + 1];
-        int i = p, j = q + 1, t = 0;
+        int i = p, j = q + 1, t = p;
 
         // 두 부분 배열을 병합
         while (i <= q && j <= r) {
@@ -59,11 +61,12 @@ public class Main {
         }
 
         // 병합 결과를 원래 배열에 저장
-        for (i = p, t = 0; i <= r; i++, t++) {
-            arr[i] = tmp[t];
+        for (i = p; i <= r; i++) {
+            arr[i] = tmp[i];
             count++;
             if (count == K) { // K번째 저장이면 결과 저장
                 result = arr[i];
+                return; // 결과를 찾으면 조기 종료
             }
         }
     }
