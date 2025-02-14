07dff2f25c1d1e37eedb167ec5c12287f7dabc55
최현기 <139421105+ggobug@users.noreply.github.com>
2025-02-15 00:06:15 +0900

    [Gold V] Title: 퇴사 2, Time: 796 ms, Memory: 322912 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 2];
+        int[] terms = new int[N];
+        int[] prices = new int[N];
+
+        String[] input;
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+            terms[i] = Integer.parseInt(input[0]);
+            prices[i] = Integer.parseInt(input[1]);
+        }
+
+        for (int i = 0; i < N; i++) {
+            // i일까지의 최대 수익 유지
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+
+            int next = i + terms[i];
+            if (next <= N) {
+                dp[next] = Math.max(dp[next], dp[i] + prices[i]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
