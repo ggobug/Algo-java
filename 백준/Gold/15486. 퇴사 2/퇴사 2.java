787b14480fefe70818baab48118d5a3c9d9a1849
최현기 <139421105+ggobug@users.noreply.github.com>
2025-02-15 00:14:24 +0900

    [Gold V] Title: 퇴사 2, Time: 652 ms, Memory: 310424 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,32 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         long[] dp = new long[N + 1];
-        int[] terms = new int[N];
-        int[] prices = new int[N];
-
-        String[] input;
-        for (int i = 0; i < N; i++) {
-            input = br.readLine().split(" ");
-            terms[i] = Integer.parseInt(input[0]);
-            prices[i] = Integer.parseInt(input[1]);
-        }
+        long maxProfit = 0;
 
         for (int i = 0; i < N; i++) {
-            // i일까지의 최대 수익 유지
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
 
-            int next = i + terms[i];
+            maxProfit = Math.max(maxProfit, dp[i]);
+
+            int next = i + t;
             if (next <= N) {
-                dp[next] = Math.max(dp[next], dp[i] + prices[i]);
+                dp[next] = Math.max(dp[next], maxProfit + p);
             }
         }
 
-        System.out.println(dp[N]);
+        System.out.println(Math.max(maxProfit, dp[N]));
     }
 }
