50082f2c6c7a7f9cd3a32597b93a0f641daa110e
최현기 <139421105+ggobug@users.noreply.github.com>
2025-07-06 14:09:59 +0900

    [Silver I] Title: 그림, Time: 376 ms, Memory: 44520 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] graph;
+    static boolean[][] visited;
+
+    static int[] dx = {0, 0, 1, -1};  // 동서남북
+    static int[] dy = {1, -1, 0, 0};
+
+    public static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
+        int area = 1;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && graph[nx][ny] == 1) {
+                        q.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+
+        return area;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0;
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && graph[i][j] == 1) {
+                    int area = bfs(i, j);
+                    cnt++;
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        System.out.println(maxArea);
+    }
+}
